28ed8d1420ca0bddf76e6475230d66db06444cb7
vcjaladi <vcjaladi@gmail.com>
2017-01-29 17:13:34 -0600

    Added more unit tests

--- TestCollatz.py
+++ TestCollatz.py
@@ -27,11 +27,23 @@ class TestCollatz (TestCase):
     # read
     # ----
 
-    def test_read(self):
+    def test_read_1(self):
         s = "1 10\n"
         i, j = collatz_read(s)
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
+
+    def test_read_2(self):
+        s = "100 200\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  100)
+        self.assertEqual(j, 200)
+
+    def test_read_3(self):
+        s = "201 210\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  201)
+        self.assertEqual(j, 210)
 
     # ----
     # eval
@@ -57,21 +69,38 @@ class TestCollatz (TestCase):
     # print
     # -----
 
-    def test_print(self):
+    def test_print_1(self):
         w = StringIO()
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
+
+    def test_print_2(self):
+        w = StringIO()
+        collatz_print(w, 100, 200, 125)
+        self.assertEqual(w.getvalue(), "100 200 125\n")
+
+    def test_print_3(self):
+        w = StringIO()
+        collatz_print(w, 201, 210, 89)
+        self.assertEqual(w.getvalue(), "201 210 89\n")
 
     # -----
     # solve
     # -----
 
-    def test_solve(self):
+    def test_solve_1(self):
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
         self.assertEqual(
             w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_2(self):
+        r = StringIO("900 1000\n201 210\n100 200\n1 10\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "900 1000 174\n201 210 89\n100 200 125\n1 10 20\n")
 
 # ----
 # main
@@ -81,7 +110,7 @@ if __name__ == "__main__":
     main()
 
 """ #pragma: no cover
-% coverage-3.5 run --branch TestCollatz.py >  TestCollatz.out 2>&1
+% T
 
 
 % cat TestCollatz.out
