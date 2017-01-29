32019ad3a88c334aa390b6fea46c82fa5e11daa4
vcjaladi <vcjaladi@gmail.com>
2017-01-29 17:26:51 -0600

    Created SphereCollatz.py and created a .gitignore file

---
+++ SphereCollatz.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2016
+# Glenn P. Downing
+# ---------------------------
+
+# ------------
+# collatz_read
+# ------------
+
+
+def collatz_read(s):
+    """
+    read two ints
+    s a string
+    return a list of two ints, representing the beginning and end of a range, [i, j]
+    """
+    a = s.split()
+    return [int(a[0]), int(a[1])]
+
+# ------------
+# collatz_eval
+# ------------
+
+
+def collatz_eval(i, j):
+    """
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    return the max cycle length of the range [i, j]
+    """
+    assert i > 0
+    assert j > i
+
+    max_cycle = 0
+    for k in range(i, j + 1):
+        num = k
+        cycle = 1
+
+        while num != 1:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num = (3 * num) + 1
+            cycle += 1
+
+        assert num == 1
+
+        if cycle >= max_cycle:
+            max_cycle = cycle
+
+    assert max_cycle > 0
+    return max_cycle
+
+# -------------
+# collatz_print
+# -------------
+
+
+def collatz_print(w, i, j, v):
+    """
+    print three ints
+    w a writer
+    i the beginning of the range, inclusive
+    j the end       of the range, inclusive
+    v the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+
+def collatz_solve(r, w):
+    """
+    r a reader
+    w a writer
+    """
+    for s in r:
+        i, j = collatz_read(s)
+        v = collatz_eval(i, j)
+        collatz_print(w, i, j, v)
+
+# -------
+# imports
+# -------
+
+import sys
+
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    collatz_solve(sys.stdin, sys.stdout)
+
+""" #pragma: no cover
+% cat RunCollatz.in
+1 10
+100 200
+201 210
+900 1000
+
+
+
+% RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+
+
+% cat RunCollatz.out
+1 10 1
+100 200 1
+201 210 1
+900 1000 1
+
+
+
+% pydoc3 -w Collatz
+# That creates the file Collatz.html
+"""
